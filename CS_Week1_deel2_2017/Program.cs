14d452229bf6835c56a0cf8883a8a6a9e696f675
swellerstone119 <tgr.jansen@gmail.com>
2017-11-17 10:08:31 +0100

    Add project files.

---
+++ CS_Week1_deel2_2017/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Week1_deel2_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
